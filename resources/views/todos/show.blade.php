02cf916b6312a1e3f99103a282b5ad8173fc2e6f
Michael Neely <michael@michaelneely.net>
2019-10-30 01:41:55 -0400

    Added Update method

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -20,8 +20,8 @@ Single Todo: {{ $todo->name }}
             <div class="card-body">
                 {{ $todo->description }}
             </div>
-
         </div>
+        <a href="/todos-app/public/todos/{{ $todo->id }}/edit" class="btn btn-info btn-sm my-2">Edit</a>
 </div>
 </div>
 
