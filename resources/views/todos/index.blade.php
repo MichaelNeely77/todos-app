91cb18675b01dcd0cadcebcfd260c973a1e95cac
Michael Neely <michael@michaelneely.net>
2019-10-31 01:12:12 -0400

    Added comlete button

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -20,7 +20,13 @@ Todos List
                                 
                                 {{ $todo->name }}
 
-                                <a href="/todos-app/public/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right">View</a>
+                                @if (!$todo->completed)
+                                    <a href="/todos-app/public/todos/{{ $todo->id }}/complete" style="color: white;" class="btn btn-primary btn-warning btn-sm float-right">Complete</a>
+                                @endif
+
+                                <a href="/todos-app/public/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right mr-2">View</a>
+
+
                             </li>
                             @endforeach
                         </ul>
